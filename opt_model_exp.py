279f729961e943cb2a457704346980d5b4cfaf3e
Bas van Stein <steinbvan@liacs.leidenuniv.nl>
2022-11-17 12:07:50 +0000

    Added models and new exp

--- opt_model_exp.py
+++ opt_model_exp.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from tensorflow.keras.utils import to_categorical
 import matplotlib.pyplot as plt
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 from sklearn.metrics import plot_confusion_matrix
 from sklearn.neural_network import MLPClassifier
@@ -11,96 +13,88 @@ import autokeras as ak
 #load data
 from BIAS.SB_Test_runner import get_scens_per_dim, get_simulated_data
 
-rep = 10000
+rep = 20000
 n_samples = 100
 
-scenes = get_scens_per_dim()
-per_label = {"unif":0, "centre":0, "bounds":0, "gaps/clusters":0, "disc":0}
-X = []
-y = []
-for scene in scenes:
-    label = scene[0]
-    kwargs = scene[1]
-    if (label == "unif"):
-        rep1 = 4 * rep
-    elif (label in ["trunc_unif", "cauchy", "norm"]):
-        rep1 = int(rep / 32)
-    elif (label in ["bound_thing","inv_norm", "inv_cauchy"]):
-        rep1 = int(rep / 48)
-    elif (label in ["clusters","gaps", "part_unif"]):
-        rep1 = int(rep / 67)
-    elif (label in ["spikes", "shifted_spikes"]):
-        rep1 = int(rep / 42)
-    data = get_simulated_data(label, rep=rep1, n_samples = n_samples, kwargs=kwargs)
-    for r in range(rep1):
-        X.append(np.sort(data[:,r]))
-    if (label in ["trunc_unif", "cauchy", "norm"]):
-        label = "centre"
-    elif (label in ["bound_thing","inv_norm", "inv_cauchy"]):
-        label = "bounds"
-    elif (label in ["gaps", "part_unif", "clusters"]):
-        label = "gaps/clusters"
-    elif (label in ["spikes", "shifted_spikes"]):
-        label = "disc"
-    per_label[label] += rep1
-    y.extend([label]*rep1)
+for n_samples in [100,200,500]:
 
-print(per_label)
-X = np.array(X)
-int_y, targetnames= pd.factorize(y)
+    scenes = get_scens_per_dim()
+    per_label = {"unif":0, "centre":0, "bounds":0, "gaps/clusters":0, "disc":0}
+    X = []
+    y = []
+    for scene in scenes:
+        label = scene[0]
+        kwargs = scene[1]
+        if (label == "unif"):
+            rep1 = 4 * rep
+        elif (label in ["trunc_unif", "cauchy", "norm"]):
+            rep1 = int(rep / 32)
+        elif (label in ["bound_thing","inv_norm", "inv_cauchy"]):
+            rep1 = int(rep / 48)
+        elif (label in ["clusters","gaps", "part_unif"]):
+            rep1 = int(rep / 67)
+        elif (label in ["spikes", "shifted_spikes"]):
+            rep1 = int(rep / 42)
+        data = get_simulated_data(label, rep=rep1, n_samples = n_samples, kwargs=kwargs)
+        for r in range(rep1):
+            X.append(np.sort(data[:,r]))
+        if (label in ["trunc_unif", "cauchy", "norm"]):
+            label = "centre"
+        elif (label in ["bound_thing","inv_norm", "inv_cauchy"]):
+            label = "bounds"
+        elif (label in ["gaps", "part_unif", "clusters"]):
+            label = "gaps/clusters"
+        elif (label in ["spikes", "shifted_spikes"]):
+            label = "disc"
+        per_label[label] += rep1
+        y.extend([label]*rep1)
 
-cat_y = to_categorical(int_y)
+    print(per_label)
+    X = np.array(X)
+    int_y, targetnames= pd.factorize(y)
+
+    cat_y = to_categorical(int_y)
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, cat_y, test_size=0.2, random_state=42, stratify=int_y)
+    X_train, X_test, y_train, y_test = train_test_split(X, cat_y, test_size=0.2, random_state=42, stratify=int_y)
 
-if True:
-    clf = ak.StructuredDataClassifier(
-        max_trials=50,
-        overwrite=True,
-    )
-else:
     clf = ak.ImageClassifier(
-        max_trials=50,
+        max_trials=100,
         overwrite=True,
     )
     X_train = np.expand_dims(X_train, axis=2)
-# Search for the best model with EarlyStopping.
-cbs = [
-    tf.keras.callbacks.EarlyStopping(patience=5),
-]
+    X_test = np.expand_dims(X_test, axis=2)
+    # Search for the best model with EarlyStopping.
+    cbs = [
+        tf.keras.callbacks.EarlyStopping(patience=5),
+    ]
 
-clf.fit(
-    x=X_train,
-    y=y_train,
-    epochs=50,
-    callbacks=cbs
-)
-# Evaluate on the testing data.
-print(
-    "Accuracy: {accuracy}".format(
-        accuracy=clf.evaluate(x=X_test, y=y_test)
+    clf.fit(
+        x=X_train,
+        y=y_train,
+        epochs=50,
+        callbacks=cbs
     )
-)
+    # Evaluate on the testing data.
+    print(
+        "Accuracy: {accuracy}".format(
+            accuracy=clf.evaluate(x=X_test, y=y_test)
+        )
+    )
 
-#interesting option from autokeras.image.image_supervised import ImageClassifier1D
-#model = ImageClassifier1D(verbose=True)
+    model = clf.export_model()
+    model.summary()
+    model.save(f"opt_cnn_model-{n_samples}.h5")
+    tf.keras.utils.plot_model(model, to_file="opt_cnn_model-{n_samples}.png")
 
-#X_train = np.expand_dims(X, axis=2)
-#model.fit(X_train, Y, time_limit=1 * 60 * 60)
+    class newmodel(MLPClassifier):
+        def __init__(self, model):
+            self.model = model
+        def predict(self, X):
+            y = self.model.predict(X)
+            return np.argmax(y, axis=1)
 
-model = clf.export_model()
-model.summary()
-model.save("opt_normal_model.h5")
-
-class newmodel(MLPClassifier):
-    def __init__(self, model):
-        self.model = model
-    def predict(self, X):
-        y = self.model.predict(X)
-        return np.argmax(y, axis=1)
-
-model1 = newmodel(model)
-fig, ax = plt.subplots(figsize=(14, 14))
-plot_confusion_matrix(model1, X_test, np.argmax(y_test, axis=1), normalize='true', xticks_rotation = 'vertical', display_labels = list(np.unique(targetnames)), ax=ax) 
-plt.savefig("confusionmatrix.png")
+    model1 = newmodel(model)
+    fig, ax = plt.subplots(figsize=(14, 14))
+    plot_confusion_matrix(model1, X_test, np.argmax(y_test, axis=1), normalize='true', xticks_rotation = 'vertical', display_labels = list(np.unique(targetnames)), ax=ax) 
+    plt.savefig("opt_cnn_model-{n_samples}-confusion.png")
