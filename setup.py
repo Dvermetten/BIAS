605080f572d773dab75d1d3253a26fda38ba2f11
Bas van Stein <steinbvan@liacs.leidenuniv.nl>
2023-11-16 09:03:41 +0000

    dimensionality fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-__version__ = "1.3.2"
+__version__ = "1.3.3"
 gh_ref = os.environ.get("GITHUB_REF")
 if gh_ref:
     *_, tag = gh_ref.split("/")
